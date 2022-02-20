a462d98bb03bc46535e88fb25ab644de68a50725
Tawhid Mirza Mahib <dhakaiyacoder@gmail.com>
2022-02-20 20:00:28 +0600

    Added User Dashboard Design with All featured locations section recyclerview

--- app/src/main/java/com/dhakaiyacoder/cityguide/User/UserDashboard.java
+++ app/src/main/java/com/dhakaiyacoder/cityguide/User/UserDashboard.java
@@ -1,16 +1,46 @@
 package com.dhakaiyacoder.cityguide.User;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
+import com.dhakaiyacoder.cityguide.HelperClasses.HomeAdapter.FeaturedAdapter;
+import com.dhakaiyacoder.cityguide.HelperClasses.HomeAdapter.FeaturedHelperClass;
 import com.dhakaiyacoder.cityguide.R;
 
+import java.util.ArrayList;
+
 public class UserDashboard extends AppCompatActivity {
+
+    RecyclerView featuredRecycler;
+
+    RecyclerView.Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_user_dashboard);
+
+        //Hooks
+        featuredRecycler = findViewById(R.id.featured_recycler);
+        featuredRecycler();
+    }
+
+    private void featuredRecycler() {
+        featuredRecycler.setHasFixedSize(true);
+        featuredRecycler.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+
+        ArrayList<FeaturedHelperClass> featuredLocations = new ArrayList<>();
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.mcdonald,"McDonald's","saofhesafhehfhfhadsofhafh ihsiphgsipdghipdsghfkahhafh gsdgjipg"));
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.city_1,"London","saofhesafhehfhfhadsofhafh ihsiphgsipdghipdsghfkahhafh gsdgjipg"));
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.city_2,"Mexico","saofhesafhehfhfhadsofhafh ihsiphgsipdghipdsghfkahhafh gsdgjipg"));
+
+        adapter = new FeaturedAdapter(featuredLocations);
+        featuredRecycler.setAdapter(adapter);
+
     }
 }
